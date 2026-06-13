ff97487583eeb447dcbd09d8d998c3e02cfac7c1
Вотинцев Евгений <evgenii_votincev92@mail.ru>
2026-06-13 20:45:13 +0500

    Update MyFlowOperators.java

--- src/homework_3/MyFlowOperators.java
+++ src/homework_3/MyFlowOperators.java
@@ -28,7 +28,7 @@ public class MyFlowOperators {
         System.out.print("Введите температуру t:");
         double t = scanner.nextDouble();
 
-        if (t > 5) {
+        if (t > -5) {
             System.out.println("Тепло");
         } else if (-5 >= t && t > -20) {
             System.out.println("Нормально");
@@ -57,25 +57,18 @@ public class MyFlowOperators {
     }
 
 
-    public static void sumNumbers() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print ("\r\n Введите целое число: ");
-        int number = scanner.nextInt();
+     public static void sumNumbers(){
+        int number;
+        do {
+            System.out.print(" \r Пожалуйста, укажите максимальное значение от 1 до максимального числа: ");
+        }while ((number = new Scanner(System.in).nextInt()) < 1);
         int sum = 0;
-        if (number > 0) {
-            for (int i = 1; i <= number; i++) {
-                sum += i;
+            for (int counter = 1; counter <= number; counter ++){
+                sum +=counter;
             }
-            System.out.println(" Сумма чисел от 1 по " + number + " равна:" + sum);
-        } else if (number == 0) {
-            System.out.println(" Сумма чисел от 1 по 0 равна: 0");
-        } else {
-            System.out.println(" Пожалуйста,введите положительно целое число.");
+            System.out.println("Cумма значений от 1 до " + number + " равно: " + sum);
         }
-
     }
-
-}
 
 
 
